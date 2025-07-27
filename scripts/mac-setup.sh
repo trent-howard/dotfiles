d9c88d92d72c26549e252dd767082dfd7a567d7f
Trent Howard <trent.howard.42@gmail.com>
2025-07-27 12:11:00 +1000

    feat(Setup): Add more system preferences

--- scripts/mac-setup.sh
+++ scripts/mac-setup.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
-# set some defaults
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# set system preferences
+# you'll need to log out and back in for a bunch of these to take effect
+
+# finder and file browsing stuff
 defaults write com.apple.finder ShowStatusBar -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write -g AppleShowAllExtensions -bool true
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 
-# Disable smart quotes and smart dashes
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-# Avoid creating .DS_Store files on network or USB volumes
+# avoid creating .DS_Store files on network or USB volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# Dock
+# ui styling
+defaults write -g AppleReduceDesktopTinting -bool true
+defaults write -g AppleInterfaceStyle -string "Dark"
+# universalaccess might need sudo or file permissions changed
+# defaults write com.apple.universalaccess reduceTransparency -bool true
+
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock showHidden -bool true # dim hidden apps in dock
 defaults write com.apple.dock tilesize -int 48
+defaults write com.apple.dock largesize -int 64
+defaults write com.apple.dock "show-recents" -bool false
 killall Dock
 
-# Disable press and hold for special characters - Enable key repeating
+# input settings
+# disable press and hold for special characters - Enable key repeating
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Set lower key repeat wait and faster repeat rate
+# set lower key repeat wait and faster repeat rate
 defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
+
+# disable smart quotes and smart dashes
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+
+# tracking speed and tap to click
+defaults write -g com.apple.trackpad.scaling -float 1.5
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
 
 # install xcode tools - we'll need these for homebrew
 if test ! $(which gcc); then
